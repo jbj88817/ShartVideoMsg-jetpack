d3ffe41c7369ae9d47483856c012de501aab3987
Bojie Jiang <jiangbojie@gmail.com>
2020-01-12 15:01:25 -0800

    PageListPlay

---
+++ app/src/main/java/com/mooc/ppjoke/exoplayer/PageListPlay.java
@@ -0,0 +1,35 @@
+package com.mooc.ppjoke.exoplayer;
+
+import android.app.Application;
+import android.view.LayoutInflater;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerControlView;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.mooc.ppjoke.R;
+
+import us.bojie.libcommon.AppGlobals;
+
+public class PageListPlay {
+
+    public SimpleExoPlayer mExoPlayer;
+    public PlayerView playerView;
+    public PlayerControlView controlView;
+
+    public PageListPlay() {
+        Application application = AppGlobals.getApplication();
+        mExoPlayer = ExoPlayerFactory.newSimpleInstance(application,
+                new DefaultRenderersFactory(application),
+                new DefaultTrackSelector(),
+                new DefaultLoadControl());
+
+        playerView = (PlayerView) LayoutInflater.from(application)
+                .inflate(R.layout.layout_exo_player_view, null, false);
+        controlView = (PlayerControlView) LayoutInflater.from(application)
+                .inflate(R.layout.layout_exo_player_controller_view, null, false);
+    }
+}
