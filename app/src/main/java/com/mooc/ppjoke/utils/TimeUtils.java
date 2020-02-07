b91ed4658ad790733ed71ee8ddf4d7bdc07b4aca
Bojie Jiang <jiangbojie@gmail.com>
2020-02-06 23:29:56 -0800

    layout_feed_detail_author_info

---
+++ app/src/main/java/com/mooc/ppjoke/utils/TimeUtils.java
@@ -0,0 +1,19 @@
+package com.mooc.ppjoke.utils;
+
+import java.util.Calendar;
+
+public class TimeUtils {
+    public static String calculate(long time) {
+        long timeInMillis = Calendar.getInstance().getTimeInMillis();
+        long diff = (timeInMillis - time) / 1000;
+        if (diff < 60) {
+            return diff + "secs ago";
+        } else if (diff < 3600) {
+            return diff / 60 + "mins ago";
+        } else if (diff < 3600 * 24) {
+            return diff / 3600 + "hours ago";
+        } else {
+            return diff / (3600 * 24) + "days ago";
+        }
+    }
+}
