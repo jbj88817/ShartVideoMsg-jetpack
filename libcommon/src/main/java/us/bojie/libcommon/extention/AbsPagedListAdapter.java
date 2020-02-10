9f4b87d52a19d9688d7615bc0ceaf6eb857cd30c
Bojie Jiang <jiangbojie@gmail.com>
2020-02-09 23:17:53 -0800

    AbsPagedListAdapter

---
+++ libcommon/src/main/java/us/bojie/libcommon/extention/AbsPagedListAdapter.java
@@ -0,0 +1,142 @@
+package us.bojie.libcommon.extention;
+
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.paging.PagedListAdapter;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+public abstract class AbsPagedListAdapter<T, VH extends RecyclerView.ViewHolder> extends PagedListAdapter<T, VH> {
+    private SparseArray<View> mHeaders = new SparseArray<>();
+    private SparseArray<View> mFooters = new SparseArray<>();
+
+    private int BASE_ITEM_VIEW_TYPE_HEADER = 100000;
+    private int BASE_ITEM_VIEW_TYPE_FOOTER = 200000;
+
+    protected AbsPagedListAdapter(@NonNull DiffUtil.ItemCallback<T> diffCallback) {
+        super(diffCallback);
+    }
+
+    public void addHeaderView(View view) {
+        if (mHeaders.indexOfValue(view) < 0) {
+            mHeaders.put(BASE_ITEM_VIEW_TYPE_HEADER++, view);
+            notifyDataSetChanged();
+        }
+    }
+
+    public void addFooterView(View view) {
+        if (mFooters.indexOfValue(view) < 0) {
+            mFooters.put(BASE_ITEM_VIEW_TYPE_FOOTER++, view);
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        int itemCount = super.getItemCount();
+        return itemCount + mHeaders.size() + mFooters.size();
+    }
+
+    public int getOriginalItemCount() {
+        return getItemCount() - mHeaders.size() - mFooters.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isHeaderPosition(position)) {
+            return mHeaders.keyAt(position);
+        }
+
+        if (isFooterPosition(position)) {
+            position = position - getOriginalItemCount() - mHeaders.size();
+            return mFooters.keyAt(position);
+        }
+
+        position = position - mHeaders.size();
+        return getItemViewType2(position);
+    }
+
+    protected abstract int getItemViewType2(int position);
+
+    private boolean isFooterPosition(int position) {
+        return position >= getOriginalItemCount() + mHeaders.size();
+    }
+
+    private boolean isHeaderPosition(int position) {
+        return position < mHeaders.size();
+    }
+
+    @NonNull
+    @Override
+    public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (mHeaders.indexOfKey(viewType) >= 0) {
+            View view = mHeaders.get(viewType);
+            return (VH) new RecyclerView.ViewHolder(view) {
+            };
+        }
+
+        if (mFooters.indexOfKey(viewType) >= 0) {
+            View view = mFooters.get(viewType);
+            return (VH) new RecyclerView.ViewHolder(view) {
+            };
+        }
+
+        return onCreateViewHolder2(parent, viewType);
+    }
+
+    protected abstract VH onCreateViewHolder2(ViewGroup parent, int viewType);
+
+    @Override
+    public void onBindViewHolder(@NonNull VH holder, int position) {
+        if (isHeaderPosition(position) || isFooterPosition(position)) {
+            return;
+        }
+
+        position = position - mHeaders.size();
+        onBindViewHolder2(holder, position);
+    }
+
+    protected abstract void onBindViewHolder2(VH holder, int position);
+
+    @Override
+    public void registerAdapterDataObserver(@NonNull RecyclerView.AdapterDataObserver observer) {
+        super.registerAdapterDataObserver(new AdapterDataObserverProxy(observer));
+    }
+
+    private class AdapterDataObserverProxy extends RecyclerView.AdapterDataObserver {
+
+        private RecyclerView.AdapterDataObserver mObserver;
+
+        public AdapterDataObserverProxy(RecyclerView.AdapterDataObserver observer) {
+            mObserver = observer;
+        }
+
+        public void onChanged() {
+            mObserver.onChanged();
+        }
+
+        public void onItemRangeChanged(int positionStart, int itemCount) {
+            mObserver.onItemRangeChanged(positionStart + mHeaders.size(), itemCount);
+        }
+
+        public void onItemRangeChanged(int positionStart, int itemCount, @Nullable Object payload) {
+            mObserver.onItemRangeChanged(positionStart + mHeaders.size(), itemCount, payload);
+        }
+
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            mObserver.onItemRangeInserted(positionStart + mHeaders.size(), itemCount);
+        }
+
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            mObserver.onItemRangeRemoved(positionStart + mHeaders.size(), itemCount);
+        }
+
+        public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+            mObserver.onItemRangeMoved(fromPosition + mHeaders.size(), toPosition + mHeaders.size(), itemCount);
+        }
+    }
+}
