f9f61c7e5041a88af1753ad42cdfbd37e5fc02fb
Bojie Jiang <jiangbojie@gmail.com>
2020-02-14 23:26:31 -0800

    comment dialog keyboard

--- app/src/main/java/com/mooc/ppjoke/ui/detail/CommentDialog.java
+++ app/src/main/java/com/mooc/ppjoke/ui/detail/CommentDialog.java
@@ -1,5 +1,6 @@
 package com.mooc.ppjoke.ui.detail;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import com.mooc.ppjoke.R;
 import com.mooc.ppjoke.databinding.LayoutCommentDialogBinding;
@@ -18,6 +20,8 @@ import com.mooc.ppjoke.ui.login.UserManager;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import us.bojie.libcommon.PixUtils;
+import us.bojie.libcommon.ViewHelper;
 import us.bojie.libnetwork.ApiResponse;
 import us.bojie.libnetwork.ApiService;
 import us.bojie.libnetwork.JsonCallback;
@@ -41,18 +45,34 @@ public class CommentDialog extends DialogFragment implements View.OnClickListene
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Window window = getDialog().getWindow();
+        window.setWindowAnimations(0);
+
         mBinding = LayoutCommentDialogBinding.inflate(inflater, container, false);
         mBinding.commentVideo.setOnClickListener(this);
         mBinding.commentDelete.setOnClickListener(this);
         mBinding.commentSend.setOnClickListener(this);
 
-        Window window = getDialog().getWindow();
         window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
 
         itemId = getArguments().getLong(KEY_ITEM_ID);
+
+        ViewHelper.setViewOutline(mBinding.getRoot(), PixUtils.dp2px(10), ViewHelper.RADIUS_TOP);
+
+        mBinding.getRoot().post(() -> showSoftInputMethod());
+
         return mBinding.getRoot();
     }
+
+    private void showSoftInputMethod() {
+        mBinding.inputView.setFocusable(true);
+        mBinding.inputView.setFocusableInTouchMode(true);
+        mBinding.inputView.requestFocus();
+        InputMethodManager manager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        manager.showSoftInput(mBinding.inputView, 0);
+    }
+
 
     @Override
     public void onClick(View v) {
