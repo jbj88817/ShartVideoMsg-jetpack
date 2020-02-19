51263117ae9ff35e2e209832a954a10e5b076108
Bojie Jiang <jiangbojie@gmail.com>
2020-02-18 22:57:51 -0800

    11-3 TagBottomSheetDialogFragment

--- app/src/main/java/com/mooc/ppjoke/ui/publish/PublishActivity.java
+++ app/src/main/java/com/mooc/ppjoke/ui/publish/PublishActivity.java
@@ -1,18 +1,102 @@
 package com.mooc.ppjoke.ui.publish;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 
 import com.mooc.ppjoke.R;
+import com.mooc.ppjoke.databinding.ActivityLayoutPublishBinding;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import us.bojie.libnavannotation.ActivityDestination;
 
 @ActivityDestination(pageUrl = "main/tabs/publish", needLogin = true)
-public class PublishActivity extends AppCompatActivity {
+public class PublishActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityLayoutPublishBinding mBinding;
+    private int width;
+    private int height;
+    private String filePath;
+    private boolean isVideo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_layout_publish);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_layout_publish);
+        mBinding.actionClose.setOnClickListener(this);
+        mBinding.actionPublish.setOnClickListener(this);
+        mBinding.actionDeleteFile.setOnClickListener(this);
+        mBinding.actionAddTag.setOnClickListener(this);
+        mBinding.actionAddFile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.action_close:
+                showExitDialog();
+                break;
+            case R.id.action_publish:
+                break;
+            case R.id.action_add_tag:
+                TagBottomSheetDialogFragment fragment = new TagBottomSheetDialogFragment();
+                fragment.setOnTagItemSelectedListener(tagList -> mBinding.actionAddTag.setText(tagList.title));
+                fragment.show(getSupportFragmentManager(), "tag_dialog");
+                break;
+            case R.id.action_add_file:
+                CaptureActivity.startActivityForResult(this);
+                break;
+            case R.id.action_delete_file:
+                mBinding.actionAddFile.setVisibility(View.VISIBLE);
+                mBinding.fileContainer.setVisibility(View.GONE);
+                mBinding.cover.setImageDrawable(null);
+                filePath = null;
+                width = 0;
+                height = 0;
+                isVideo = false;
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CaptureActivity.REQ_CAPTURE && resultCode == RESULT_OK && data != null) {
+            width = data.getIntExtra(CaptureActivity.RESULT_FILE_WIDTH, 0);
+            height = data.getIntExtra(CaptureActivity.RESULT_FILE_HEIGHT, 0);
+            filePath = data.getStringExtra(CaptureActivity.RESULT_FILE_PATH);
+            isVideo = data.getBooleanExtra(CaptureActivity.RESULT_FILE_TYPE, false);
+
+            showFileThumbnail();
+        }
+    }
+
+    private void showFileThumbnail() {
+        if (TextUtils.isEmpty(filePath)) {
+            return;
+        }
+
+        mBinding.actionAddFile.setVisibility(View.GONE);
+        mBinding.fileContainer.setVisibility(View.VISIBLE);
+        mBinding.cover.setImageUrl(filePath);
+        mBinding.videoIcon.setVisibility(isVideo ? View.VISIBLE : View.GONE);
+        mBinding.cover.setOnClickListener(v -> PreviewActivity
+                .startActivityForResult(PublishActivity.this, filePath, isVideo, null));
+    }
+
+    private void showExitDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage(getString(R.string.publish_exit_message))
+                .setNegativeButton(getString(R.string.cancel), null)
+                .setPositiveButton(getString(R.string.exit), (dialog, which) -> {
+                    dialog.dismiss();
+                    PublishActivity.this.finish();
+                }).create().show();
     }
 }
---
+++ app/src/main/java/com/mooc/ppjoke/ui/publish/TagBottomSheetDialogFragment.java
@@ -0,0 +1,135 @@
+package com.mooc.ppjoke.ui.publish;
+
+import android.app.Dialog;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.mooc.ppjoke.R;
+import com.mooc.ppjoke.databinding.LayoutTagBottomSheetDialogBinding;
+import com.mooc.ppjoke.model.TagList;
+import com.mooc.ppjoke.ui.login.UserManager;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.arch.core.executor.ArchTaskExecutor;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import us.bojie.libcommon.utils.PixUtils;
+import us.bojie.libnetwork.ApiResponse;
+import us.bojie.libnetwork.ApiService;
+import us.bojie.libnetwork.JsonCallback;
+
+import static us.bojie.libcommon.utils.ToastUtils.showToast;
+
+public class TagBottomSheetDialogFragment extends BottomSheetDialogFragment {
+    private LayoutTagBottomSheetDialogBinding mBinding;
+    private RecyclerView recyclerView;
+    private TagsAdapter adapter;
+    private List<TagList> mTagLists;
+    private OnTagItemSelectedListener listener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        mBinding = DataBindingUtil.inflate(LayoutInflater.from(getContext()),
+                R.layout.layout_tag_bottom_sheet_dialog, null, false);
+        recyclerView = mBinding.recyclerView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new TagsAdapter();
+        recyclerView.setAdapter(adapter);
+
+        View root = mBinding.getRoot();
+        dialog.setContentView(root);
+        ViewGroup parent = (ViewGroup) root.getParent();
+        BottomSheetBehavior<ViewGroup> behavior = BottomSheetBehavior.from(parent);
+        behavior.setPeekHeight(PixUtils.getScreenHeight() / 3);
+        behavior.setHideable(false);
+
+        ViewGroup.LayoutParams params = parent.getLayoutParams();
+        params.height = PixUtils.getScreenHeight() / 3 * 2;
+        parent.setLayoutParams(params);
+
+        queryTagList();
+        return dialog;
+    }
+
+    private void queryTagList() {
+        ApiService.get("/tag/queryTagList")
+                .addParam("userId", UserManager.get().getUserId())
+                .addParam("pageCount", 100)
+                .addParam("tagId", 0)
+                .execute(new JsonCallback<List<TagList>>() {
+                    @Override
+                    public void onSuccess(ApiResponse<List<TagList>> response) {
+
+                        if (response.body != null) {
+                            List<TagList> body = response.body;
+                            mTagLists.clear();
+                            mTagLists.addAll(body);
+                            ArchTaskExecutor.getMainThreadExecutor().execute(() -> adapter.notifyDataSetChanged());
+                        }
+                    }
+
+                    @Override
+                    public void onError(ApiResponse<List<TagList>> response) {
+                        showToast(response.message);
+                    }
+                });
+    }
+
+    class TagsAdapter extends RecyclerView.Adapter {
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            TextView textView = new TextView(parent.getContext());
+            textView.setTextSize(13);
+            textView.setTypeface(Typeface.DEFAULT_BOLD);
+            textView.setGravity(Gravity.CENTER_VERTICAL);
+            textView.setTextColor(ContextCompat.getColor(parent.getContext(), R.color.color_000));
+            textView.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup
+                    .LayoutParams.MATCH_PARENT, PixUtils.dp2px(45)));
+            return new RecyclerView.ViewHolder(textView) {
+            };
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            TextView textView = (TextView) holder.itemView;
+            TagList tagList = mTagLists.get(position);
+            textView.setText(tagList.title);
+
+            holder.itemView.setOnClickListener(v -> {
+                if (listener != null) {
+                    listener.onTagItemSelected(tagList);
+                    dismiss();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mTagLists.size();
+        }
+    }
+
+    public void setOnTagItemSelectedListener(OnTagItemSelectedListener listener) {
+        this.listener = listener;
+    }
+
+    interface OnTagItemSelectedListener {
+        void onTagItemSelected(TagList tagList);
+    }
+}
