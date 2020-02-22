7842e7f107e36f0c5b109646f76053c9816dca15
Bojie Jiang <jiangbojie@gmail.com>
2020-02-21 22:13:44 -0800

    fragment me layout

--- app/src/main/java/com/mooc/ppjoke/utils/StringConvert.java
+++ app/src/main/java/com/mooc/ppjoke/utils/StringConvert.java
@@ -1,10 +1,27 @@
 package com.mooc.ppjoke.utils;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+
 public class StringConvert {
     public static String convertNum(int count) {
         if (count < 1000) {
             return String.valueOf(count);
         }
         return count / 1000 + "k";
+    }
+
+    public static String convertSpannable(int count, String intro) {
+        String countStr = String.valueOf(count);
+        SpannableString ss = new SpannableString(countStr + intro);
+        ss.setSpan(new ForegroundColorSpan(Color.BLACK), 0, countStr.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new AbsoluteSizeSpan(16, true), 0, countStr.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new StyleSpan(Typeface.BOLD), 0, countStr.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return ss.toString();
     }
 }
